4df601101257b9edcc71d46b2da64b6215fb115b
sharmamarketingandsuppliers <sharmamarketingandsuppliers@gmail.com>
2024-07-25 11:44:31 +0545

    Add files via upload

--- contact.js
+++ contact.js
@@ -12,18 +12,3 @@ $(document).ready(function () {
     $("#cross").hide();
   });
 });
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
