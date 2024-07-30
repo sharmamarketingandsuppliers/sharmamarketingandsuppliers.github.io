d4ef6c1a4c09d20f548c8985e433595e188a3bf1
sharmamarketingandsuppliers <sharmamarketingandsuppliers@gmail.com>
2024-07-30 07:53:45 +0545

    Update product.js

--- product.js
+++ product.js
@@ -11,24 +11,7 @@ $(document).ready(function () {
       $("#cross").hide();
     });
   });
-  
-  function itemSearchFunction() {
-    var input, filter, div, column, p;
-    input = document.getElementById("itemSearch");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("box");
-    column = div.getElementsByClassName("column");
-    for (i = 0; i < column.length; i++) {
-      p = column[i].getElementsByTagName("p")[0];
-      if (p.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        column[i].style.display = "";
-      } else {
-        column[i].style.display = "none";
-      }
-    }
-  }
-  
-  // script.js
+
 document.addEventListener('DOMContentLoaded', () => {
   const slider = document.querySelector('.slider');
   const items = document.querySelectorAll('.slider-item');
